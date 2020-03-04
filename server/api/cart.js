c96e788986da8fdb3a4f7c573875c3fa5b2647cd
TLLong <tllongdev@gmail.com>
2020-03-04 17:34:40 -0600

    api/cart get/:userId res.json changed to type object

--- server/api/cart.js
+++ server/api/cart.js
@@ -11,7 +11,7 @@ router.get("/:userId", async (req, res, next) => {
     if (userCart) {
       res.json(userCart);
     } else {
-      res.json([]);
+      res.json({});
     }
   } catch (err) {
     next(err);
