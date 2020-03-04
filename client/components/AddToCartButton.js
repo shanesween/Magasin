c39d6415a836aed3cca48e7011a842b4244709e4
lizzya821 <58485970+lizzya821@users.noreply.github.com>
2020-03-04 15:53:50 -0600

    Update AddToCartButton.js

--- client/components/AddToCartButton.js
+++ client/components/AddToCartButton.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const AddToCartButton = () => {
   return (
-    <button type="button" class="btn btn-outline-success">
+    <button type="button" className="btn btn-outline-success">
       Add to your cart
     </button>
   );
