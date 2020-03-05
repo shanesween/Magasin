ec6079d994fe89b7dae058761ffcf24b67c8db64
Jovanni Morales <jnmrles@gmail.com>
2020-03-05 17:42:59 -0600

    merge conflict

--- client/components/Review.jsx
+++ client/components/Review.jsx
@@ -1,9 +1,4 @@
 import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { fetchReviews } from '../store/review';
-import AddToCartButton from './AddToCartButton';
-import SingleReview from './SingleReviews';
-import { fetchProduct } from '../store/singleProduct';
 
 const Review = props => {
   const reviews = props.product.reviews;
