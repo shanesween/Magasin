f76ab657f446c1d256c977835e01f9cca7ba2f61
lizzya821 <lizzya821@gmail.com>
2020-03-05 09:58:59 -0600

    changed file extensions for components and if statement in cart hook

--- client/components/Cart.jsx
+++ client/components/Cart.jsx
@@ -1,27 +1,26 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchCart } from '../store/cart';
-import CartItem from './CartItem';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchCart } from "../store/cart";
+import CartItem from "./CartItem";
 
 const Cart = () => {
   const dispatch = useDispatch();
   const cart = useSelector(state => state.cart);
   const user = useSelector(state => state.user);
-  console.log(user);
-    useEffect(() => {
-      dispatch(fetchCart(user.id));
-    }, [user]);
+  useEffect(() => {
+    user.id && dispatch(fetchCart(user.id));
+  }, [user]);
   return cart.products ? (
-    <div className='justify-content-center m-2'>
+    <div className="justify-content-center m-2">
       {/* <Fade cascade> */}
-      <div className='d-flex justify-content-between mt-2 ml-4 mr-4'>
+      <div className="d-flex justify-content-between mt-2 ml-4 mr-4">
         <cite>
-          <h3 className='mt-3'>Shopping Cart</h3>
+          <h3 className="mt-3">Shopping Cart</h3>
         </cite>
       </div>
       {/* </Fade> */}
-      <div className='d-inline-block'>
-        <div className='align-content-around flex-wrap row m-2'>
+      <div className="d-inline-block">
+        <div className="align-content-around flex-wrap row m-2">
           {cart.products.map(cartItem => {
             return <CartItem key={cartItem.id} cartItem={cartItem} />;
           })}
@@ -29,9 +28,9 @@ const Cart = () => {
       </div>
     </div>
   ) : (
-    <div className='container-fluid text-center mb-mt-2'>
+    <div className="container-fluid text-center mb-mt-2">
       {/* <Fade> */}
-      <p className='p-3'>There are no items in your cart.</p>
+      <p className="p-3">There are no items in your cart.</p>
       {/* </Fade> */}
     </div>
   );
