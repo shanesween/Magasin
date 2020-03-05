a86b5d577b672377ffaa2467db764cf413ba08f1
Shane Sweeney <shanep.sween@gmail.com>
2020-03-05 10:33:11 -0600

    Product modal, Single Product updated, added details button to product component, other cleanup

--- client/store/singleProduct.js
+++ client/store/singleProduct.js
@@ -1,27 +1,35 @@
-import axios from 'axios'
+import axios from "axios";
 
-const SET_PRODUCT = 'SET_PRODUCT'
+//ACTION TYPES
+
+const SET_PRODUCT = "SET_PRODUCT";
+
+// ACTION CREATORS
 
 const setProduct = product => {
-  return {type: SET_PRODUCT, product}
-}
+  return { type: SET_PRODUCT, product };
+};
+
+// THUNK CREATORS
 
 export const fetchProduct = id => {
   return async dispatch => {
     try {
-      const {data} = await axios.get(`/api/products/${id}`)
-      dispatch(setProduct(data))
+      const { data } = await axios.get(`/api/products/${id}`);
+      dispatch(setProduct(data));
     } catch (err) {
-      console.log(err)
+      console.log(err);
     }
-  }
-}
+  };
+};
+
+// REDUCER
 
 export default function(product = {}, action) {
   switch (action.type) {
     case SET_PRODUCT:
-      return action.product
+      return action.product;
     default:
-      return product
+      return product;
   }
 }
