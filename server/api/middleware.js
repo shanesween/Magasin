40ff3697c8e1854771809e8a13df1fad37fbf067
Shane Sweeney <shanep.sween@gmail.com>
2020-03-10 17:51:50 -0500

    Pagination attempt

--- server/api/middleware.js
+++ server/api/middleware.js
@@ -13,40 +13,4 @@ const checkAdmin = async (req, res, next) => {
   }
 };
 
-// const loggedIn = async (req, res, next) => {
-//   if (!req.user) {
-//     if (!req.session.cart) {
-//       let order = await Order.create();
-//       req.session.cartId = order.id;
-//     }
-//     next();
-//   } else {
-//     // let user = await User.findByPk(req.user.id)
-//     // user.googleId = req.session.id
-//     // await user.save()
-//     next();
-//   }
-// };
-
-// const stockCheck = async (req, res, next) => {
-//   const { token } = req.body;
-//   let orderItems = await OrderItem.findAll({
-//     where: { orderId: token.cartId }
-//   });
-
-//   let productsArray = orderItems.filter(async orderItem => {
-//     let productFound = await Product.findByPk(orderItem.productId);
-//     // productFound
-//     if (Number(orderItem.quantity) > Number(productFound.stock)) {
-//       return productFound.id;
-//     }
-//   });
-//   console.log("productsArray", productsArray);
-//   if (productsArray.length) {
-//     res.sendStatus(400);
-//   } else {
-//     next();
-//   }
-// };
-
 module.exports = { checkAdmin };
--- server/api/users.js
+++ server/api/users.js
@@ -6,9 +6,6 @@ module.exports = router;
 router.get("/", checkAdmin, async (req, res, next) => {
   try {
     const users = await User.findAll({
-      // explicitly select only the id and email fields - even though
-      // users' passwords are encrypted, it won't help if we just
-      // send everything to anyone who asks!
       order: [["id", "ASC"]],
       attributes: ["id", "email", "isAdmin", "address"]
     });
