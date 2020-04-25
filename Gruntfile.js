6e8c0cf48a0cddfe6ce3cbe34cdc3997afb1111e
Shane Sweeney <shanep.sween@gmail.com>
2020-04-25 15:12:58 -0400

    Adding sass

---
+++ Gruntfile.js
@@ -0,0 +1,21 @@
+module.exports = (grunt) => {
+  grunt.initConfig({
+    sass: {
+      development: {
+        options: {},
+        files: {
+          "public/main.css": "public/main.scss",
+        },
+      },
+    },
+    watch: {
+      files: ["<%= sass.files %>"],
+      tasks: ["sass"],
+    },
+  });
+  grunt.loadNpmTasks("grunt-contrib-sass");
+  grunt.loadNpmTasks("grunt-contrib-watch");
+
+  grunt.registerTask("default", ["sass"]);
+  grunt.registerTask("heroku:production", "sass");
+};
